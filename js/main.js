506d9ad8dd29b1ed5e3efef16174184f08af95f2
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2025-02-25 23:30:29 +0700

    add comment

--- js/main.js
+++ js/main.js
@@ -7,21 +7,81 @@ navTriggerBtn.addEventListener('click', () => {
 
 const swiper = new Swiper('.swiper', {
     loop: true,
-    pagination:{
-        el:'.swiper-pagination',
+    pagination: {
+        el: '.swiper-pagination',
         clickable: true,
     },
     slidesPerView: 3,
     spaceBetween: 20,
     breakpoints: {
-        320:{
+        320: {
             slidesPerView: 1,
         },
-        768:{
+        768: {
             slidesPerView: 2,
         },
-        960:{
+        960: {
             slidesPerView: 3,
         }
     }
 });
+
+const sr = ScrollReveal({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 3000,
+    delay: 600,
+});
+
+sr.reveal('.hero__text', {
+    origin: 'top',
+})
+sr.reveal('.steps__step', {
+    distance: '100px',
+    interval: 100
+})
+sr.reveal('.about__text', {
+    origin: 'left',
+})
+sr.reveal('.about__img', {
+    origin: 'right',
+    delay: 800
+})
+sr.reveal('.testimonial__bg', {
+    delay: 800
+})
+sr.reveal('.testimonial__title', {
+    delay: 800
+})
+sr.reveal('.testimonial__slider', {
+    delay: 1000
+})
+sr.reveal('.brands__img', {
+    delay: 600,
+    interval: 100,
+    distance: '100px'
+})
+sr.reveal('.work__title')
+sr.reveal('.work__subtitle', {
+    delay: 800
+})
+sr.reveal('.work__grid', {
+    delay: 100
+})
+sr.reveal('.stats')
+sr.reveal('.stats__item', {
+    distance: '100px',
+    interval: 100
+})
+sr.reveal('.news__title')
+sr.reveal('.news__subtitle', {
+    delay: 800
+})
+sr.reveal('.news__item', {
+    distance: '100px',
+    interval: 100,
+    delay: 1000
+})
+
+sr.reveal('.contact__container')
+sr.reveal('.contact__text', {delay:800})
